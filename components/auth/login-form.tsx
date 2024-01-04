86648b508cd14219f1c4e39b702091381c08ed0e
jirh <myworld710@163.com>
2024-01-04 17:38:35 +0800

    87

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,14 +3,67 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { LoginSchema } from "@/schemas";
 
-import { Form } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { CardWrapper } from "./card-wrapper";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      account: "",
+      password: "",
+    },
+  });
+  const { handleSubmit, control } = form;
   return (
     <CardWrapper>
-      <Form></Form>
+      <Form {...form}>
+        <form onSubmit={handleSubmit((data) => {})}>
+          <div className=" space-y-6">
+            <FormField
+              control={control}
+              name="account"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>用户名</FormLabel>
+                  <FormControl>
+                    <Input placeholder="用户名" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>密码</FormLabel>
+                  <FormControl>
+                    <Input placeholder="密码" {...field} type="password" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit">登录</Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
 export const LoginSchema = z.object({
-  account: z.string(),
+  account: z.string().min(1, { message: "请输入用户名" }),
   password: z.string().min(6),
 });
